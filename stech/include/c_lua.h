b32efdd7ddd138469358cae5ac9858daab6c5ce2
Fernando Nogueira <mrfernando2509@gmail.com>
2021-09-16 21:14:03 -0400

    refactor: changed strcat_s to strcat

--- stech/include/c_lua.h
+++ stech/include/c_lua.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <raylib.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
--- stech/src/c_lua.c
+++ stech/src/c_lua.c
@@ -1,39 +1,41 @@
-#include <raylib.h>
 #include "c_lua.h"
 
-
-void StackDump(lua_State *L) {
+void StackDump(lua_State *L)
+{
     int i;
     int top = lua_gettop(L);
-    for (i = 1; i <= top; i++) {  /* repeat for each level */
+    for (i = 1; i <= top; i++)
+    { /* repeat for each level */
         int t = lua_type(L, i);
-        switch (t) {
+        switch (t)
+        {
 
-            case LUA_TSTRING:  /* strings */
-                printf("`%s'", lua_tostring(L, i));
-                break;
+        case LUA_TSTRING: /* strings */
+            printf("`%s'", lua_tostring(L, i));
+            break;
 
-            case LUA_TBOOLEAN:  /* booleans */
-                printf(lua_toboolean(L, i) ? "true" : "false");
-                break;
+        case LUA_TBOOLEAN: /* booleans */
+            printf(lua_toboolean(L, i) ? "true" : "false");
+            break;
 
-            case LUA_TNUMBER:  /* numbers */
-                printf("%g", lua_tonumber(L, i));
-                break;
+        case LUA_TNUMBER: /* numbers */
+            printf("%g", lua_tonumber(L, i));
+            break;
 
-            default:  /* other values */
-                printf("%s", lua_typename(L, t));
-                break;
+        default: /* other values */
+            printf("%s", lua_typename(L, t));
+            break;
         }
-        printf("  ");  /* put a separator */
+        printf("  "); /* put a separator */
     }
-    printf("\n");  /* end the listing */
+    printf("\n"); /* end the listing */
 }
 
-
-void DoString(lua_State *L, const char *str) {
+void DoString(lua_State *L, const char *str)
+{
     int r = luaL_dostring(L, str);
-    if (r != LUA_OK) {
+    if (r != LUA_OK)
+    {
         const char *error = lua_tostring(L, -1);
         printf("%s\n", error);
         lua_pop(L, 1);
@@ -41,9 +43,10 @@ void DoString(lua_State *L, const char *str) {
     }
 }
 
-
-bool PushField(lua_State *L, const char *field) {
-    if (!lua_istable(L, -1)) {
+bool PushField(lua_State *L, const char *field)
+{
+    if (!lua_istable(L, -1))
+    {
         printf("top is not lua table!\n");
         return false;
     }
@@ -51,29 +54,32 @@ bool PushField(lua_State *L, const char *field) {
     return true;
 }
 
-void QuickPushVariable(lua_State *L, const char *variable) {
+void QuickPushVariable(lua_State *L, const char *variable)
+{
     char cmd[128] = "_internal_temp=";
-    strcat_s(cmd, 128, variable);
+    strcat(cmd, variable);
     DoString(L, cmd);
     lua_getglobal(L, "_internal_temp");
 }
 
-void ConsoleWrite(lua_State *L, const char *str) {
+void ConsoleWrite(lua_State *L, const char *str)
+{
     QuickPushVariable(L, "console.write");
     lua_pushstring(L, str);
     if (lua_pcall(L, 1, 0, 0) != 0)
         printf("There was an error talking with the log module, %s\n", lua_tostring(L, -1));
 }
 
-void ConsoleLog(lua_State *L, const char *str) {
+void ConsoleLog(lua_State *L, const char *str)
+{
     QuickPushVariable(L, "console.log");
     lua_pushstring(L, str);
     if (lua_pcall(L, 1, 0, 0) != 0)
         printf("There was an error talking with the log module, %s\n", lua_tostring(L, -1));
 }
 
-
-const char *GetConsoleBuffer(lua_State *L) {
+const char *GetConsoleBuffer(lua_State *L)
+{
     lua_getglobal(L, "console");
     lua_getfield(L, -1, "screen_buffer");
     const char *str = lua_tostring(L, -1);
@@ -81,8 +87,8 @@ const char *GetConsoleBuffer(lua_State *L) {
     return str;
 }
 
-
-Vector2 GetVector2(lua_State *L, int position) {
+Vector2 GetVector2(lua_State *L, int position)
+{
     lua_getfield(L, position, "x");
     lua_getfield(L, position, "y");
     Vector2 vec = {lua_tonumber(L, -2), lua_tonumber(L, -1)};
@@ -90,7 +96,8 @@ Vector2 GetVector2(lua_State *L, int position) {
     return vec;
 }
 
-Vector3 GetVector3(lua_State *L, int position) {
+Vector3 GetVector3(lua_State *L, int position)
+{
     lua_getfield(L, position, "x");
     lua_getfield(L, position, "y");
     lua_getfield(L, position, "z");
@@ -99,7 +106,8 @@ Vector3 GetVector3(lua_State *L, int position) {
     return vec;
 }
 
-Vector4 GetVector4(lua_State *L, int position) {
+Vector4 GetVector4(lua_State *L, int position)
+{
     lua_getfield(L, position, "x");
     lua_getfield(L, position, "y");
     lua_getfield(L, position, "z");
@@ -109,7 +117,8 @@ Vector4 GetVector4(lua_State *L, int position) {
     return vec;
 }
 
-Rectangle GetRect(lua_State *L, int position) {
+Rectangle GetRect(lua_State *L, int position)
+{
     lua_getfield(L, position, "x");
     lua_getfield(L, position, "y");
     lua_getfield(L, position, "w");
@@ -119,16 +128,18 @@ Rectangle GetRect(lua_State *L, int position) {
     return rect;
 }
 
-int luaDrawRect(lua_State *L) {
-    Rectangle rect = (Rectangle) {lua_tointeger(L, 1), lua_tointeger(L, 2), lua_tointeger(L, 3), lua_tointeger(L, 4)};
-    Color col = (Color) {lua_tointeger(L, 5), lua_tointeger(L, 6), lua_tointeger(L, 7), lua_tointeger(L, 8)};
+int luaDrawRect(lua_State *L)
+{
+    Rectangle rect = (Rectangle){lua_tointeger(L, 1), lua_tointeger(L, 2), lua_tointeger(L, 3), lua_tointeger(L, 4)};
+    Color col = (Color){lua_tointeger(L, 5), lua_tointeger(L, 6), lua_tointeger(L, 7), lua_tointeger(L, 8)};
     DrawRectangleRec(rect, col);
     return 1;
 }
 
-int luaDrawText(lua_State *L) {
+int luaDrawText(lua_State *L)
+{
     // Text - x - y - font size - color
-    Color col = (Color) {lua_tointeger(L, 5), lua_tointeger(L, 6), lua_tointeger(L, 7), lua_tointeger(L, 8)};
+    Color col = (Color){lua_tointeger(L, 5), lua_tointeger(L, 6), lua_tointeger(L, 7), lua_tointeger(L, 8)};
     DrawText(lua_tostring(L, 1), lua_tointeger(L, 2), lua_tointeger(L, 3), lua_tointeger(L, 4), col);
     return 1;
 }
