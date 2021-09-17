96a7c12e96262c8108d59325ad98bb5d07e17d6c
Fernando Nogueira <mrfernando2509@gmail.com>
2021-09-16 21:11:52 -0400

    remove: raygui dep

--- sedit/src/main.c
+++ sedit/src/main.c
@@ -1,28 +1,26 @@
 #include "raylib.h"
 
-#define RAYGUI_IMPLEMENTATION
-#define RAYGUI_SUPPORT_RICONS
-
-#include "r_raygui.h"
-
-#undef RAYGUI_IMPLEMENTATION
-
 #include "c_lua.h"
 
 #define CMD_BUFFER_SIZE 255
 #define FONT_SIZE 8
 
-bool SInputLine(int x, int y, char *text, int buffSize, bool editMode) {
-    if (editMode) {
-        int key = GetCharPressed();      // Returns codepoint as Unicode
-        int keyCount = (int) strlen(text);
+bool SInputLine(int x, int y, char *text, int buffSize, bool editMode)
+{
+    if (editMode)
+    {
+        int key = GetCharPressed(); // Returns codepoint as Unicode
+        int keyCount = (int)strlen(text);
         bool bufferNotFull = keyCount < (buffSize - 1);
         // Only allow keys in range [32..125]
-        if (bufferNotFull && key >= 32) {
+        if (bufferNotFull && key >= 32)
+        {
             text[keyCount] = key;
             keyCount++;
             text[keyCount] = '\0';
-        } else if (buffSize > 0 && IsKeyPressed(KEY_BACKSPACE)) {
+        }
+        else if (buffSize > 0 && IsKeyPressed(KEY_BACKSPACE))
+        {
             keyCount--;
             text[keyCount] = '\0';
         }
@@ -33,7 +31,8 @@ bool SInputLine(int x, int y, char *text, int buffSize, bool editMode) {
 
 static char consoleInputEntry[CMD_BUFFER_SIZE] = "";
 
-int main() {
+int main()
+{
 
     int screenWidth = 690;
     int screenHeight = 560;
@@ -53,25 +52,27 @@ int main() {
     lua_setglobal(L, "draw_text");
 
     int error = luaL_dofile(L, "data/core/lualib/init.lua");
-    if (error == LUA_OK) {
+    if (error == LUA_OK)
+    {
         printf("Lua initialized\n");
-    } else {
+    }
+    else
+    {
         ConsoleLog(L, lua_tostring(L, -1));
     }
 
-
     bool wantsToClose = false;
     // Main game loop
-    while (!wantsToClose)    // Detect window close button or ESC key
+    while (!wantsToClose) // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
         wantsToClose = WindowShouldClose();
 
-        if (IsKeyPressed(KEY_GRAVE)) {
+        if (IsKeyPressed(KEY_GRAVE))
+        {
             StackDump(L);
         }
-
 
         //----------------------------------------------------------------------------------
 
@@ -82,12 +83,12 @@ int main() {
 
         // UI Stuff
 
-
         // Console stuff
         // TODO Move console to UI itself
 
         DrawText(">", 0, 0, FONT_SIZE, BLACK);
-        if (SInputLine(FONT_SIZE, 0, consoleInputEntry, 255, true)) {
+        if (SInputLine(FONT_SIZE, 0, consoleInputEntry, 255, true))
+        {
             ConsoleWrite(L, "]");
             ConsoleLog(L, consoleInputEntry);
             DoString(L, consoleInputEntry);
@@ -110,7 +111,7 @@ int main() {
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     lua_close(L);
